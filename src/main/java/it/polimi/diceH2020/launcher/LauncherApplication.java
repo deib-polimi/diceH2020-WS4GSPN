181af77f1a546ba622c8769f965db2d25f2b24f9
jacoporigoli <jackeape@gmail.com>
2016-04-21 18:23:02 +0200

    MultifileUpload Set Tomcat maxSwallowSize

--- src/main/java/it/polimi/diceH2020/launcher/LauncherApplication.java
+++ src/main/java/it/polimi/diceH2020/launcher/LauncherApplication.java
@@ -1,11 +1,13 @@
 package it.polimi.diceH2020.launcher;
 
+import org.apache.catalina.connector.Connector;
 import org.apache.log4j.Logger;
 import org.h2.server.web.WebServlet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,6 +17,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 
 import it.polimi.diceH2020.launcher.utility.FileUtility;
 
@@ -27,6 +30,7 @@ import it.polimi.diceH2020.launcher.utility.FileUtility;
 @EnableScheduling
 public class LauncherApplication {
 
+	private static final int uploadTomcatMaxSize = -1; //for a correct redirect disable tomcat. set -1 to disable preupload check
 	private static Logger logger = Logger.getLogger(LauncherApplication.class.getName());
 
 	@Autowired
@@ -41,6 +45,19 @@ public class LauncherApplication {
 		} catch (Exception e) {
 			logger.info("Error in the creation of local work directory!");
 		}
+	}
+	
+	@Bean
+	public TomcatEmbeddedServletContainerFactory containerFactory() {
+	    return new TomcatEmbeddedServletContainerFactory() {
+	        protected void customizeConnector(Connector connector) {
+	            super.customizeConnector(connector);
+	            if (connector.getProtocolHandler() instanceof AbstractHttp11Protocol) {
+	                ((AbstractHttp11Protocol <?>) connector.getProtocolHandler()).setMaxSwallowSize(uploadTomcatMaxSize);
+	            }
+	        }
+	    };
+
 	}
 	
 	@Bean
--- src/main/java/it/polimi/diceH2020/launcher/controller/UploadExceptionHandler.java
+++ src/main/java/it/polimi/diceH2020/launcher/controller/UploadExceptionHandler.java
@@ -1,16 +1,37 @@
 package it.polimi.diceH2020.launcher.controller;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MultipartException;
-import org.springframework.web.servlet.ModelAndView;
+
 
 @ControllerAdvice
 public class UploadExceptionHandler {
+	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(UploadExceptionHandler.class.getName());
+	
+//	@ExceptionHandler(MaxUploadSizeExceededException.class)
+//    public ModelAndView handleMaxUploadSizeExceededException(){
+//    	logger.info("Max file size exceeded");
+//		ModelAndView modelAndView = new ModelAndView();
+//		modelAndView.addObject("message", "Max file size exceeded");
+//		modelAndView.setViewName("error");
+//		return modelAndView;
+//    }
+	
 	@ExceptionHandler(MultipartException.class)
-	  public ModelAndView handleError(MultipartException exception) {
-	    ModelAndView modelAndView = new ModelAndView();
-	    modelAndView.addObject("message", exception.getMessage());
-	    modelAndView.setViewName("error");
-	    return modelAndView;
-	  }
+	public String handleError(MultipartException exception,Model modelAndView) {
+		logger.info("MultipartUploadException: "+ exception);
+	    modelAndView.addAttribute("message", exception);
+	    return "error";
+	}
+	
+//	@ExceptionHandler(value = Exception.class)
+//    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception exception) throws Exception {
+//		ModelAndView modelAndView = new ModelAndView();
+//	    modelAndView.addObject("message", exception.getMessage());
+//	    modelAndView.setViewName("error");
+//        return modelAndView;
+//    }
 }
